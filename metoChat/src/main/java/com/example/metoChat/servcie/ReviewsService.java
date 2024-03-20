8b77256cc62d30e431caa81fa0458c0ec432f53f
lee <>
2024-03-20 17:24:29 +0900

    review 조회 기능 백엔드

--- metoChat/src/main/java/com/example/metoChat/servcie/ReviewsService.java
+++ metoChat/src/main/java/com/example/metoChat/servcie/ReviewsService.java
@@ -2,36 +2,30 @@ package com.example.metoChat.servcie;
 
 import com.example.metoChat.domain.Reviews.Reviews;
 import com.example.metoChat.domain.Reviews.ReviewsRepository;
-import com.example.metoChat.domain.mentor.Mentor;
+import com.example.metoChat.domain.metoring.Mentoring;
 import com.example.metoChat.domain.user.User;
-import com.example.metoChat.domain.user.UserRepository;
 import com.example.metoChat.exception.CustomException;
+import com.example.metoChat.web.dto.Review.ReviewsListResponseDto;
 import com.example.metoChat.web.dto.Review.SaveReivewRqeustDto;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.Optional;
-
 import static com.example.metoChat.exception.ErrorCode.REIVEW_NOT_FOUND;
-import static com.example.metoChat.exception.ErrorCode.USER_NOT_FOUND;
 
 @RequiredArgsConstructor
 @Service
 public class ReviewsService {
 
     private final ReviewsRepository reviewsRepository;
-    private final MentorService mentorService;
-    private final UserService userService;
 
     // 리뷰 등록
     @Transactional
-    public Long saveReview(SaveReivewRqeustDto reivewRqeustDto, String email){
-
-        User user = userService.getUserByEmail(email);
-        Mentor mentor = mentorService.getMentor(reivewRqeustDto.getMentorId());
-        Reviews reviews = reviewsRepository.save(reivewRqeustDto.toEntity(user, mentor ));
-
+    public Long saveReview(SaveReivewRqeustDto reivewRqeustDto, User user, Mentoring mentoring){
+        Reviews reviews = reviewsRepository.save(reivewRqeustDto.toEntity(user, mentoring ));
         return reviews.getId();
     }
 
@@ -40,12 +34,16 @@ public class ReviewsService {
     public Long updateReview(SaveReivewRqeustDto reivewRqeustDto, Long reveiwId){
         Reviews reviews = reviewsRepository.findById(reveiwId)
                 .orElseThrow(() -> new CustomException(REIVEW_NOT_FOUND));
+
+        reviews.update(reivewRqeustDto);
         return reviews.getId();
     }
 
     // 리뷰 목록 조회
-
-    // 내 리뷰 목록 조회
-
-    // 리뷰 조회
+    @Transactional(readOnly = true)
+    public Page<ReviewsListResponseDto> getReviewsList(Pageable pageable , Long id, int state){
+        int page = pageable.getPageNumber() - 1; // page 위치에 있는 값은 0부터 시작
+        int pageLimit = 5; // 한페이지에 보여줄 글 개수
+        return reviewsRepository.getReviewList(PageRequest.of(page, pageLimit), id, state);
+    }
 }
--- metoChat/src/main/java/com/example/metoChat/web/ReivewController.java
+++ metoChat/src/main/java/com/example/metoChat/web/ReivewController.java
@@ -3,7 +3,13 @@ package com.example.metoChat.web;
 import com.example.metoChat.config.auth.LoginUser;
 import com.example.metoChat.config.auth.dto.SessionUser;
 import com.example.metoChat.domain.Reviews.Reviews;
+import com.example.metoChat.domain.mentor.Mentor;
+import com.example.metoChat.domain.metoring.Mentoring;
+import com.example.metoChat.domain.user.User;
+import com.example.metoChat.servcie.MentorService;
+import com.example.metoChat.servcie.MentoringService;
 import com.example.metoChat.servcie.ReviewsService;
+import com.example.metoChat.servcie.UserService;
 import com.example.metoChat.web.dto.HttpResponseDto;
 import com.example.metoChat.web.dto.Review.SaveReivewRqeustDto;
 import lombok.RequiredArgsConstructor;
@@ -18,21 +24,28 @@ import org.springframework.web.bind.annotation.*;
 public class ReivewController {
 
     private final ReviewsService reviewsService;
+    private final UserService userService;
+    private final MentoringService mentoringService;
 
     // 사용자 등록
     @PostMapping("/")
     public ResponseEntity save(@RequestBody SaveReivewRqeustDto reivewRqeustDto, @LoginUser SessionUser user) {
         HttpHeaders httpHeaders = new HttpHeaders();
-        Long reviewId =  reviewsService.saveReview(reivewRqeustDto, user.getEmail());
+        User findUser = userService.getUserByEmail(user.getEmail());
+        Mentoring mentoring = mentoringService.getMentoring(reivewRqeustDto.getMemtoringId());
+        Long reviewId =  reviewsService.saveReview(reivewRqeustDto, findUser, mentoring);
+
         return new ResponseEntity<>(new HttpResponseDto(true, reviewId), httpHeaders, HttpStatus.OK);
     }
 
     // 사용자 수정
     @PutMapping("/{reviewID}")
-    public ResponseEntity stateUpdate(@RequestBody SaveReivewRqeustDto reivewRqeustDto, @PathVariable("reviewID") Long reviewID, @LoginUser SessionUser user) {
+    public ResponseEntity stateUpdate(@RequestBody SaveReivewRqeustDto reivewRqeustDto, @PathVariable("reviewID") Long reviewID) {
         HttpHeaders httpHeaders = new HttpHeaders();
         Long reviewId = reviewsService.updateReview(reivewRqeustDto, reviewID);
         return new ResponseEntity<>(new HttpResponseDto(true, reviewId), httpHeaders, HttpStatus.OK);
 
     }
+
+
 }
